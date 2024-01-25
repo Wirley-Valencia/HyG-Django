9394cad1d74baa00196fb2c89ae0831fb9c5b2be
Ederson-RL <edersonramirezlopez@gmail.com>
2024-01-24 23:11:08 -0500

    Checkpoint video 14

--- carts/models.py
+++ carts/models.py
@@ -5,6 +5,8 @@ from user.models import CustomUser
 from products.models import Product
 
 from django.db.models.signals import pre_save
+from django.db.models.signals import m2m_changed
+import decimal
 
 
 class Cart (models.Model):
@@ -17,8 +19,30 @@ class Cart (models.Model):
     total = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    FEE = 0.05
+
     def __str__(self):
         return self.cart_id
+
+    def update_totals(self):
+        self.update_subtotal()
+        self.update_total()
+
+    def update_subtotal(self):
+        self.subtotal = sum([product.price for product in self.products.all()])
+        self.save()
+
+    def update_total(self):
+        self.total = self.subtotal + \
+            (self.subtotal * decimal.Decimal(Cart.FEE))
+        self.save()
+
+
+class CartProducts (models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 def set_cart_id(sender, instance, *args, **kwargs):
@@ -26,4 +50,10 @@ def set_cart_id(sender, instance, *args, **kwargs):
         instance.cart_id = str(uuid.uuid4())
 
 
+def update_totals(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.update_totals()
+
+
 pre_save.connect(set_cart_id, sender=Cart)
+m2m_changed.connect(update_totals, sender=Cart.products.through)
--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,8 @@ from .utils import get_or_create_cart
 # Create your views here.
 from .models import Cart
 from products.models import Product
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 
 
 def cart(request):
@@ -16,10 +18,18 @@ def cart(request):
 
 def add(request):
     cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
 
     cart.products.add(product)
 
     return render(request, 'carts/add.html', {
         'product': product
     })
+
+
+def remove(request):
+    cart = get_or_create_cart(request)
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+
+    cart.products.remove(product)
+    return redirect('carts:cart')
