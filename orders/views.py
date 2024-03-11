ffe4732094a4c184821aa4206097fe89659a85d4
Ederson-RL <edersonramirezlopez@gmail.com>
2024-03-11 02:08:29 -0500

    Commit pre pull confimacion de pedido

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ from carts.utils import get_or_create_cart
 from .models import Order
 from .utils import get_or_create_order
 from .utils import breadcrumb
-
+from django.shortcuts import redirect
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 
@@ -13,8 +13,30 @@ def order(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart, request)
 
-    return render(request, 'order.html', {
+    return render(request, 'orders/order.html', {
         'cart': cart,
         'order': order,
         'breadcrumb': breadcrumb()
+    })
+
+
+@login_required(login_url='login')
+# @validate_cart_and_order
+def confirm(request):
+
+    # if not cart.has_products() or order.billing_profile is None:
+    #     return redirect('carts:cart')
+
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart, request)
+
+    # shipping_address = order.shipping_address
+    # if shipping_address is None:
+    #     return redirect('orders:address')
+
+    return render(request, 'orders/confirm.html', {
+        'cart': cart,
+        'order': order,
+        # 'shipping_address': shipping_address,
+        'breadcrumb': breadcrumb(confirmation=True)
     })
