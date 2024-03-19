25222fa3fa6520a7ccaa052d683681b66c798f21
IvanR3n3 <ivanrene.jimenezbuitrago.123@gmail.com>
2024-03-19 15:53:31 -0500

    7 commit de despliegue

--- proyectohyg/settings.py
+++ proyectohyg/settings.py
@@ -189,7 +189,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 if not DEBUG:
