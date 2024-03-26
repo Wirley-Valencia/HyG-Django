9c0946e60f9730b3381e6985c944de8d77352ece
Wirley-Valencia <vanessavalencia1052@gmail.com>
2024-03-25 21:27:16 -0500

    arreglo ultimo2

--- proyectohyg/settings.py
+++ proyectohyg/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#%93$h0mycryc+fv=%qfsmu9wrs0@(2&y&7)4ysfxdm_##^50&'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hyg-django2.onrender.com']
 
