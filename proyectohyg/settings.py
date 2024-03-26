c12cc5ffb05f5003950acf78b1d2b4fa7f4ca0d5
Wirley-Valencia <vanessavalencia1052@gmail.com>
2024-03-25 21:32:51 -0500

    arreglo ultimo3

--- proyectohyg/settings.py
+++ proyectohyg/settings.py
@@ -185,11 +185,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Utiliza os.path.join() para unir las partes de la ruta
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
