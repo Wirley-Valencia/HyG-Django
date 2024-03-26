94092d49398a0991c7cee63a1e2a5c1a17fef11d
Wirley-Valencia <vanessavalencia1052@gmail.com>
2024-03-25 21:15:15 -0500

    arreglo3

--- proyectohyg/settings.py
+++ proyectohyg/settings.py
@@ -185,9 +185,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-import os
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -198,8 +197,6 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 
 
 # Default primary key field type
